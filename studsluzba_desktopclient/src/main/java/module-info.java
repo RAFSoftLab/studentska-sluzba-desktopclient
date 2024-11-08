536d085f2735f8424ed7e7c1905627e7e63be3b7
Bojana DS <bdimicsurla@raf.rs>
2024-11-08 13:50:01 +0100

    moduli

---
+++ studsluzba_desktopclient/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module artikli {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.base;
+    exports app;
+    exports grupa1;
+    exports grupa1.model;
+    exports grupa2;
+    exports avgust.model;
+    exports avgust;
+}
